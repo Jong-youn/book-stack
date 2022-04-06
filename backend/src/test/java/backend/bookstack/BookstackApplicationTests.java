5fc3f6345397afadb81970c1b9022ab1ff8ad3d4
ljy6816@gmail.com <ljy6816@gmail.com>
2022-04-06 09:17:03 +0900

    setting

---
+++ backend/src/test/java/backend/bookstack/BookstackApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.bookstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
